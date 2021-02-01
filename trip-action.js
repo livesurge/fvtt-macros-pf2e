a07190eb07ba7d7757995d3f718cd5cdb9106b43
Steven C <6942488+DataSyncID@users.noreply.github.com>
2021-02-01 16:36:17 -0500

    Delete trip-action.js

--- trip-action.js
+++
@@ -1,8 +0,0 @@
-if (actor && actor.isPC) {
-    const options = actor.getRollOptions(['all', 'str-based', 'skill-check', 'athletics']);
-    options.push("trip");
-    options.push("attack");
-    actor.data.data.skills.ath.roll(event, options);
-} else {
-    ui.notifications.warn("You must select a PC to use this Skill Action Macro.");
-}
